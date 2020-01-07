2cdc70bb5df744e1b24c9a474e30e66d2f88286d
João Leite <joaopleite26@gmail.com>
2020-01-07 05:24:30 -0300

    Initial Commit

---
+++ MovieFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieFinder
+//
+//  Created by João Leite on 07/01/20.
+//  Copyright © 2020 João Leite. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
